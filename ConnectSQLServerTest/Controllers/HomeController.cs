f0bbb5e25ba0a6e3988837a3cce4cbbce8f4b46b
GOLF_PANACHAI_NIYOMKODCHAKORN <panachai.ny@gmail.com>
2017-07-22 00:23:09 +0700

    test database

--- ConnectSQLServerTest/Controllers/HomeController.cs
+++ ConnectSQLServerTest/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ConnectSQLServerTest.Models;
 
 namespace ConnectSQLServerTest.Controllers {
     public class HomeController : Controller {
